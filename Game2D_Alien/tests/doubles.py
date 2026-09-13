8f8261378923f7caef12c77a78254e7021d65a35
João Pedro <depgon2402@gmail.com>
2026-09-13 19:08:10 -0300

    Códigos exemplo de teste para o projeto.

--- Game2D_Alien/tests/doubles.py
+++ Game2D_Alien/tests/doubles.py
@@ -1,5 +1,26 @@
 from src.desconto import IDesconto
 
+
 class StubSemDesconto(IDesconto):
     def calcular(self, valor: float) -> float:
-        return 0  # Retorna sempre 0, simulando um cenário sem desconto
+        return 0
+
+
+# STUB - Tela (não renderiza)
+class StubScreen:
+    def __init__(self):
+        self.width = 800
+        self.height = 600
+    
+    def blit(self, image, rect):
+        pass
+    
+    def fill(self, color):
+        pass
+    
+    def get_rect(self):
+        class Rect:
+            right = 800
+            midbottom = (400, 600)
+        return Rect()
+
