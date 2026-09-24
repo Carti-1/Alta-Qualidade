40c05a87bd2940206cfe395a69423ca55962ab51
Carti-1 <depgon2402@gmail.com>
2026-09-24 11:41:17 -0300

    refactor: implementação da camada de serviços.

--- Desconto_app/src/main.py
+++ Desconto_app/src/main.py
@@ -5,25 +5,33 @@ if __package__ in (None, ""):
     root = Path(__file__).resolve().parent.parent
     sys.path.insert(0, str(root))
 
+from src.repositories.pedido_repository import PedidoRepository
+from src.controllers.pedido_controller import PedidoController
 from src.services.pedido_service import PedidoService
+from src.database.connection import DatabaseConnection
 from src.models.desconto import DescontoVIP, DescontoNormal, DescontoPremium
 from src.models.pedido import Pedido
 
 if __name__ == "__main__":
-    service = PedidoService()
+    database = DatabaseConnection()
+    repo = PedidoRepository(database)
+    service = PedidoService(repo)
+    controller = PedidoController(service)
 
-    """Criando pedidos e aplicando descontos"""
-    pedido1 = Pedido("Cliente A", DescontoNormal())
-    pedido1.valor_original = 100.0
-
-    pedido2 = Pedido("Cliente B", DescontoVIP())
+    pedido2 = Pedido(cliente="Cliente B", desconto= DescontoVIP())
     pedido2.valor_original = 200.0
 
-    pedido3 = Pedido("Cliente C", DescontoPremium())
+    pedido3 = Pedido(cliente="Cliente C", desconto= DescontoPremium())
     pedido3.valor_original = 300.0
 
-    service.adicionar_pedido(pedido1)
-    service.adicionar_pedido(pedido2)
-    service.adicionar_pedido(pedido3)
+    controller.adicionar_pedido(pedido1)
+    controller.adicionar_pedido(pedido2)
+    controller.adicionar_pedido(pedido3)
 
-    service.processar_pedidos()
+    controller.processar_pedidos()
+
+    pedidos = repo.listar_pedidos()
+
+    for pedido in pedidos:
+        print(f"Cliente: {pedido.cliente}")
+        print(f"Valor final: {pedido.valor_final(pedido.valor_original)}")
